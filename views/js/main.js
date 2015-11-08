38ed3c7639735591d78f4960e2c099914075f797
John Meas <johnkmeas@gmail.com>
2015-11-08 03:15:43 -0800

    delete jquery cdn

--- views/js/main.js
+++ views/js/main.js
@@ -536,7 +536,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 6; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizzasm.png";
+    elem.src = "images/pizza.png";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
